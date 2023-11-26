858ffbe339dddb2a32c3f3f5b62089a7e8fcacd9
Aldo <aldo@MacBook-Pro-de-Aldo-2.local>
2023-11-25 22:35:19 -0600

    funcionando con coors

--- src/components/FormNote.js
+++ src/components/FormNote.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import serviceNote from '../services/notes'
 
 export default function FormNote() {
+   
   return (
-    <div>FormNote</div>
+    <>
+   {/* <form action={handleSubmit}>
+        <div>
+            <input onChange={handleChange} type="text" />
+            <button type="">Guardar</button>
+        </div>
+    </form> */}
+    </>
   )
 }
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,1 +1,21 @@
-const axios = require('axios')
+import axios from 'axios'
+const base_url ='https://notes-api-backend-git-master-aldorodriguez28.vercel.app/api/notes/'
+
+const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    }
+  };
+
+const getAllNotes = async ()=>{
+    const response = await axios.get(base_url,config)
+    const {data} = response
+    return data
+}
+
+const createNote =async (object_note)=>{
+    const response = await axios.post(base_url, object_note, config)
+    return response.data
+}
+export default {getAllNotes, createNote}
